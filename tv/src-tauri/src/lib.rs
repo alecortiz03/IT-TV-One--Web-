869d9de4acdec983cb598374c6e3c91f3ad3956e
alecortiz03 <alecortiz03@gmail.com>
2026-09-16 12:06:08 -0600

    Tauri update fix

--- tv/src-tauri/src/lib.rs
+++ tv/src-tauri/src/lib.rs
@@ -518,18 +518,21 @@ pub fn run() {
     tauri::Builder::default()
 
         .setup(|app| {
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(
-                            log::LevelFilter::Info,
-                        )
-                        .build(),
-                )?;
-            }
+    if cfg!(debug_assertions) {
+        app.handle().plugin(
+            tauri_plugin_log::Builder::default()
+                .level(log::LevelFilter::Info)
+                .build(),
+        )?;
+    }
 
-            Ok(())
-        })
+    // Start Axum server in the background
+    tauri::async_runtime::spawn(async {
+        start_web_server().await;
+    });
+
+    Ok(())
+})
 
         .invoke_handler(
             tauri::generate_handler![
